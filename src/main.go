09d3cf0d3f20958f6119c860ed91de0fb3a43e58
kvo <kvo@envs.net>
2024-04-22 12:20:46 +0930

    src/main.go: add option to query with reverse of the flashcards

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -9,32 +10,47 @@ import (
 
 // add option to output incorrect terms to a certain file
 
+var (
+	rflag bool
+)
+
+func init() {
+	flag.BoolVar(&rflag, "r", false, "query with reverse of the flashcard")
+}
+
 func main() {
-	if len(os.Args) != 2 {
+	flag.Parse()
+	if len(flag.Args()) != 1 {
 		fmt.Fprintln(os.Stderr, "memorize: invalid invocation")
 		os.Exit(1)
 	}
-	file, err := os.Open(os.Args[1])
+	file, err := os.Open(flag.Arg(0))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "memorize: cannot open %s: %v\n", os.Args[1], err)
 		os.Exit(1)
 	}
+	i := 0
+	j := 1
 	var guess string
 	terms := bufio.NewScanner(file)
 	scanner := bufio.NewScanner(os.Stdin)
+	if rflag {
+		i = 1
+		j = 0
+	}
 	for terms.Scan() {
 		pair := strings.Split(terms.Text(), "\t")
-		fmt.Printf("%s: ", pair[0])
+		fmt.Printf("%s: ", pair[i])
 		scanner.Scan()
 		guess = scanner.Text()
-		if guess == pair[1] {
+		if guess == pair[j] {
 			fmt.Println("Correct!")
 		} else {
-			fmt.Println(pair[1])
+			fmt.Println(pair[j])
 		}
 	}
 	if err := terms.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "memorize: error reading %s: %v", os.Args[1], err)
+		fmt.Fprintf(os.Stderr, "memorize: error reading %s: %v", flag.Arg(1), err)
 		os.Exit(1)
 	}
 }
